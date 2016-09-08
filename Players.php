79fb8ab5f73700ee1b9df58f82ddea8cdf0bc895
Tsani Nashrullah <tsani.nashrullah@gmail.com>
2016-09-08 17:01:36 +0100

    Fix bugs and error (not finish)

--- Players.php
+++ Players.php
@@ -19,25 +19,36 @@ class Player {
 	public function get_mana() {
 		return $this->mana;
 	}
-}
+	public function attack() {
+     $mana = $mana - 10;
+	}
+	public function defend() {
+		$blood = $blood - 30;
+	}
 
+}
+$x = 1;
 $players = [];
+do{
 echo "
 # ============================== #
 # Welcome to the Battle Arena #
 # ------------------------------------------------- ---- #\n
 # Description: #
-# 1 type 'new' to create a character #
-# 2. type 'start' to begin the fight #
+# 1. type 'new' to create a character #
+# 2. type 'quit' to exit the application #
 # ------------------------------------------------- ---- #\n";
 echo "\n";
-
-fscanf(STDIN, "%s\n", $input_mode);
 echo "# Current Player : " . count($players, COUNT_RECURSIVE) . " #\n
 # - #
 # * Max player 2 or 3 #
 # ------------------------------------------------- ---- #\n \n";
+// var_dump($players);
+// var_dump(count($players, COUNT_RECURSIVE)<3);
+
+fscanf(STDIN, "%s\n", $input_mode);
 if ($input_mode=="new") {
+	if (count($players, COUNT_RECURSIVE)<3) {
 	echo "# ============================== #
 # Welcome to the Battle Arena #
 # ------------------------------------------------- ---- #
@@ -48,31 +59,62 @@ if ($input_mode=="new") {
 # Insert the Number of players : ";
 	// integer $number_players;
 	fscanf(STDIN, "%s\n", $number_players);
-	// if (is_int($number_players)) {
-		for ($i=0; $i < $number_players ; $i++) { 
-		echo "\n -";
-		fscanf(STDIN, "%s\n", $input_name);
-		$players[$input_name] = new Player($input_name);
-		}
-	// } else {
-	    // echo "Variable is not an integer";
-	// }
-	var_dump(is_int($number_players));
-	var_dump(is_int(1));
-	var_dump($number_players);
+		if ($number_players > 3 && count($players, COUNT_RECURSIVE) < 3) {
+			echo "The Players is too much";
+		}else{			
+			for ($i=0; $i < $number_players ; $i++) { 
+			echo "\n - ";
+			fscanf(STDIN, "%s\n", $input_name);
+			$players[$input_name] = new Player($input_name);
+			}
 echo " #\n";
 echo "# Current Player : ";
 echo count($players, COUNT_RECURSIVE);
-echo "\n# - #
+echo "\n# - 
 # * Max player 2 or 3 #
 # ------------------------------------------------- ---- #\n \n";
-fscanf(STDIN, "%s\n", $input_mode);
+		}
+	}else{
+		echo "\n \nThe Player has sign is too much\n \n \n";
+	}
+	// if (is_int($number_players)) {
+	// } else {
+	    // echo "Variable is not an integer";
+	// }
+// die();
 } elseif ($input_mode=="start") {
-	# code...
+foreach ($players[$input_name] as $value) {
+	echo $value->name;
+}
+var_dump($players[$input_name]);
+echo "# ============================== #
+# Welcome to the Battle Arena #
+# ------------------------------------------------- ---- #
+Battle Start:
+who will attack: ";
+	fscanf(STDIN, "%s\n", $scan_player);
+	if ($scan_player==$players[$input_name]) {
+		
+	}
+
+// fscanf(STDIN, "%s\n", $number_players);
+// who attacked
+// : <Nama_player_2>
+// when pressing enter out the results:
+// # ============================== #
+// # Welcome to the Battle Arena #
+// # ------------------------------------------------- ---- #
+// Battle Start:
+// who will attack: <nama_player_1>
+// who attacked: <nama_player_2>
+// Description:
+// <Nama_player_1>: manna = <rest>, blood = <rest>
+// <Nama_player_2>: manna = <rest>, blood = <rest>
 } else {
 	echo "Your input is " . $input_mode . "\n Please make sure with your input\n";
+	// die();
 }
-echo "\n\n";
+}while($x>=0);
 // var_dump($players["Albert"]->get_name());
 // $tsan = JOptionPane.showInputDialog("Masukkan Jam Kerja : ");
 ?>
